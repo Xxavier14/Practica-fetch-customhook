77790435e8889b6ef7ba8b96a37f2be16a22b71d
Juan P Javier <juanpablojavier14@gmail.com>
2024-03-03 20:48:07 -0300

    cutom hooks

---
+++ src/hooks/catImage.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+const CAT_PREFIX_IMAGE_URL = `https://cataas.com/cat/`
+
+
+export  function useCatImage ({ fact }) {
+    const [imageUrl, setImageUrl] = useState()
+
+    useEffect(() => {
+        if (!fact) return
+    
+        const threeFirstWords = fact.split(' ', 3).join(' ');
+        console.log(threeFirstWords);
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?json=true`)
+          .then(res => res.json())
+          .then(data => {
+            const { _id } = data;
+            setImageUrl(`${CAT_PREFIX_IMAGE_URL}${_id}/says/${threeFirstWords}?fontSize=50&fontColor=white`);
+          })
+    
+      }, [fact]);
+      return { imageUrl }
+}
