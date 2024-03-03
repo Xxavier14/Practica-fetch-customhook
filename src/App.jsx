9b4671bfb8b97c310ee58983c889ef1940eab5af
Juan P Javier <juanpablojavier14@gmail.com>
2024-03-03 20:45:07 -0300

    app and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useCatImage } from './hooks/catImage.jsx'
+import { useCatFact } from './hooks/catFact.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { fact, refreshCatFact}  = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
+  
+  const handleClick = async () => {
+    refreshCatFact(); // Add parentheses to call the function
+  }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main >
+      <h1>Datos de gatitos:</h1>
+
+      {fact && <p>{ fact }</p>}
+
+      <button onClick={handleClick}>Nuevo dato</button>
+
+      {imageUrl && <img src={imageUrl} alt='image'></img>}
+    </main>
   )
 }
 
